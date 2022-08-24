d1e1b27ec0b47a344dc63d6f2399916dcb780c1b
Bahadur_mia <bahadurmia7@gmail.com>
2022-08-25 05:40:21 +0600

    all is ok !

---
+++ main.js
@@ -0,0 +1,5 @@
+const open = document.getElementById('open')
+const close= document.getElementById('close')
+const modal = document.getElementById('modal')
+
+open.addEventListener('click', ()=> modal.classList.add('show-modal'))
